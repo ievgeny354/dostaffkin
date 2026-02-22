81cd1657c54f8f488793f5cf5a63f994755e8561
ievgeny354 <ievgeny354@gmail.com>
2026-02-22 14:05:28 +0300

    Add files via upload

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { Header } from '../../header/header';
+import { RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-home',
-  imports: [],
-  templateUrl: './home.html',
-  styleUrl: './home.css',
+selector: 'app-home',
+imports: [Header, RouterLink],
+templateUrl: './home.html',
+styleUrl: './home.css',
 })
 export class Home {
 
